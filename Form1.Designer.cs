1b533e043285be75be68af370da22ecbd3aead82
zhyssen <98223521+zhyssen@users.noreply.github.com>
2022-03-27 16:18:29 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace PasswordValidation
+{
+    partial class PasswordValidationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PasswordValidationForm));
+            this.requirementTitleLabel = new System.Windows.Forms.Label();
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.checkPasswordButton = new System.Windows.Forms.Button();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // requirementTitleLabel
+            // 
+            this.requirementTitleLabel.AutoSize = true;
+            this.requirementTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.requirementTitleLabel.Location = new System.Drawing.Point(22, 37);
+            this.requirementTitleLabel.Name = "requirementTitleLabel";
+            this.requirementTitleLabel.Size = new System.Drawing.Size(365, 112);
+            this.requirementTitleLabel.TabIndex = 0;
+            this.requirementTitleLabel.Text = resources.GetString("requirementTitleLabel.Text");
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionLabel.Location = new System.Drawing.Point(109, 177);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(178, 24);
+            this.instructionLabel.TabIndex = 1;
+            this.instructionLabel.Text = "Enter a password.";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTextBox.Location = new System.Drawing.Point(26, 220);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(361, 26);
+            this.passwordTextBox.TabIndex = 2;
+            // 
+            // checkPasswordButton
+            // 
+            this.checkPasswordButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkPasswordButton.Location = new System.Drawing.Point(77, 274);
+            this.checkPasswordButton.Name = "checkPasswordButton";
+            this.checkPasswordButton.Size = new System.Drawing.Size(105, 56);
+            this.checkPasswordButton.TabIndex = 4;
+            this.checkPasswordButton.Text = "Check Password";
+            this.checkPasswordButton.UseVisualStyleBackColor = true;
+            this.checkPasswordButton.Click += new System.EventHandler(this.checkPasswordButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(223, 274);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(105, 56);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Exit\r\n";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // PasswordValidationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 358);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.checkPasswordButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.instructionLabel);
+            this.Controls.Add(this.requirementTitleLabel);
+            this.Name = "PasswordValidationForm";
+            this.Text = "Password Validation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label requirementTitleLabel;
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button checkPasswordButton;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordValidation
+{
+    public partial class PasswordValidationForm : Form
+    {
+        public PasswordValidationForm()
+        {
+            InitializeComponent();
+        }
+
+        // NumberUppercase function checks to make sure there is an uppercase
+        // character in the password
+        private int NumberUpperCase (string str)
+        {
+            int upperCase = 0;      // Number of uppercase characters
+
+            // Count the number of uppercase characters
+            foreach (char ch in str)
+            {
+                if (char.IsUpper(ch))
+                {
+                    upperCase++;
+                }
+            }
+            return upperCase;
+        }
+
+        // NumberLowercase function checks to make sure there is a lowercase
+        // character in the password
+        private int NumberLowerCase(string str)
+        {
+            int lowerCase = 0;      // Number of lowercase characters
+
+            // Count the number of lowercase characters
+            foreach (char ch in str)
+            {
+                if (char.IsLower(ch))
+                {
+                    lowerCase++;
+                }
+            }
+            return lowerCase;
+        }
+
+        // NumberNumeric function checks to make sure there is a numberic
+        // character in the password
+        private int NumberNumeric(string str)
+        {
+            int digit = 0;      // Number of numeric characters
+
+            // Count the number of numeric characters
+            foreach (char ch in str)
+            {
+                if (char.IsDigit(ch))
+                {
+                    digit++;
+                }
+            }
+            return digit;
+        }
+
+        // NumberSpecial function checks to make sure there is a special
+        // character in the password
+        private int NumberSpecial(string str)
+        {
+            int special = 0;      // Number of special characters
+
+            // Count the number of special characters
+            foreach (char ch in str)
+            {
+                if (char.IsSymbol(ch))
+                {
+                    special++;
+                }
+            }
+            return special;
+        }
+
+        private void checkPasswordButton_Click(object sender, EventArgs e)
+        {
+            const int MIN_LENGTH = 8;       // Minimum length for the password
+
+            // Get the password from the textbox
+            string password = passwordTextBox.Text;
+
+            // Validate the password
+            if (password.Length >= MIN_LENGTH && NumberUpperCase(password) >= 1 && 
+                NumberLowerCase(password) >= 1 && NumberNumeric(password) >= 1 && 
+                NumberSpecial(password) >= 1)
+            {
+                MessageBox.Show("The password is valid");
+            }
+            else
+            {
+                MessageBox.Show("The password does not meet the requirements.");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form
+            this.Close();
+        }
+    }
+}
